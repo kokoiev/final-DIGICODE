eeea335f30766c788ff7b015c7a1e1eb988d1496
Treedomhouse Account <treedom2019@gmail.com>
2021-12-03 17:03:23 +0200

    first server

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+async function start() {
+  const PORT = process.env.PORT || 5555;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-}
-bootstrap();
+
+  await app.listen(PORT, () => console.log(`my server started on PORT: ${PORT}`))
+};
+
+
+start();
